8b754b110e0e9d200b4ffceda30898889ed02890
Aldair <adrianaaylinmaldonadosiles@gmail.com>
2026-06-05 07:43:01 -0400

    pagina boda

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,13 +1,9 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl =https://dxazxsbuzeqilgsodvxh.supabase.co
-
-const supabaseKey =sb_publishable_rVBL0VZr5O-e9u-Ch6TQPA_qDRGZSpJ
-
-console.log('URL:', supabaseUrl)
-console.log('KEY:', supabaseKey)
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
 export const supabase = createClient(
-  supabaseUrl!,
-  supabaseKey!
+  supabaseUrl,
+  supabaseKey
 )
